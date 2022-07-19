4093c1410d14cb576c3fdfb8355f206e344eafe7
Dean De Block <dean.de.block@gmail.com>
2022-07-19 16:21:11 +0200

    remove unused decorator

--- app/serializers/spree/v2/storefront/taxon_serializer_decorator.rb
+++
@@ -1,6 +0,0 @@
-module Spree::Api::V2::Storefront::TaxonSerializerDecorator
-  Spree::V2::Storefront::TaxonSerializer.class_eval do
-    attribute :products_count
-    attribute :available_brands
-  end
-end
