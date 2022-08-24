0050ccdf6b80af1d7007334e09fc04ec2a3fa2bb
Dean De Block <dean.de.block@gmail.com>
2022-08-24 08:02:56 +0200

    add filter param to the cache key

---
+++ app/controllers/concerns/spree/api/v2/caching_decorator.rb
@@ -0,0 +1,34 @@
+module Concerns::Spree::Api::V2
+  Spree::Api::V2::Caching.class_eval do
+    extend ActiveSupport::Concern
+
+    def collection_cache_key(collection)
+      ids_and_timestamps = collection.unscope(:includes).unscope(:order).pluck(:id, :updated_at)
+
+      ids = ids_and_timestamps.map(&:first)
+      max_updated_at = ids_and_timestamps.map(&:last).max
+
+      cache_key_parts = [
+        self.class.to_s,
+        max_updated_at,
+        ids,
+        resource_includes,
+        sparse_fields,
+        serializer_params,
+        params[:sort]&.strip,
+        params[:page]&.to_s&.strip,
+        params[:per_page]&.to_s&.strip,
+        params[:filter]&.to_s&.strip,
+      ].flatten.join('-')
+
+      Digest::MD5.hexdigest(cache_key_parts)
+    end
+
+    def collection_cache_opts
+      {
+        namespace: Spree::Api::Config[:api_v2_collection_cache_namespace],
+        expires_in: Spree::Api::Config[:api_v2_collection_cache_ttl],
+      }
+    end
+  end
+end
